c7c70cf2f7511b73acd3b35ed2cec6f3fa1de271
Onur Güzel <ongguzel@gmail.com>
2022-07-29 00:55:14 +0300

    SignalR entegrasyonu ve konfigürasyonu.

---
+++ Presentation/API/Hubs/MessageHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace API.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("receiveMessage", message);
+        }
+    }
+}
--- Presentation/API/Program.cs
+++ Presentation/API/Program.cs
@@ -1,15 +1,20 @@
+using API.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
     policy.AllowCredentials()
           .AllowAnyHeader().
           AllowAnyMethod()
           .SetIsOriginAllowed(x => true)));
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.UseCors();
 app.MapControllers();
+app.MapHub<MessageHub>("/messagehub");
 
 app.Run();
