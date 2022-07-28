881cd9bf135f7db4f25b3c7097612aab7bf57d70
Onur Güzel <ongguzel@gmail.com>
2022-07-28 16:25:47 +0300

    API projesine Controller eklendi ve program.cs ayarlandı.

---
+++ Presentation/API/Controllers/MessageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(string message)
+        {
+            return Ok();
+        }
+    }
+}
