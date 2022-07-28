41f9ffb8b703825512c780d1264067323fdd6fa1
Onur Güzel <ongguzel@gmail.com>
2022-07-28 22:02:52 +0300

    API ile RabbitMQ'ya mesaj gönderildi.

--- Presentation/API/Controllers/MessageController.cs
+++ Presentation/API/Controllers/MessageController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
 
 namespace API.Controllers
 {
@@ -7,9 +9,18 @@ namespace API.Controllers
     [ApiController]
     public class MessageController : ControllerBase
     {
-        [HttpPost]
+        [HttpPost("{message}")]
         public IActionResult Post(string message)
         {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://tzstczei:GFtaGndKJ2gCgdSyMbP01EozG2edaOOy@toad.rmq.cloudamqp.com/tzstczei");
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare("messageQueue", false, false, false);
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "messageQueue", body: data); 
+
             return Ok();
         }
     }
